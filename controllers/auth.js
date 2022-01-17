f85ced5218adb9651e45e2d4827dda5c3471e42b
JDemn <dsoftwaremancilla@outlook.es>
2022-01-16 20:26:52 -0600

    model data base, restringir no entrar usuarios repetidos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,34 +1,50 @@
 const { response } = require('express');
+const Usuario = require('../model/Usuario'); //vamos usar el modelo de db
 
-
-const CREATE_USER = ( req, res = response)=>{  //express.response es para recuperar la ayuda del tipado de node
-
+const CREATE_USER = (req, res = response) => {  //express.response es para recuperar la ayuda del tipado de node
     const { name, email, password } = req.body;
-    //validaciones con express validator > manejo de errores con express validator. all in middlewares   
-    res.status( 201 ).json({
-        ok : true,
-        msj :'new',
-        name,
-        email,
-        password
-    });
+    try {
+        
+        let usuario = Usuario.findOne({email:email});
+        console.log(usuario)
+        if(usuario){
+            return res.status( 400 ).json({
+                ok : false,
+                msj : 'Este usuario ya existe en la base de datos'
+            })
+        } 
+        usuario = new Usuario(req.body);
+        usuario.save();
+        
+        //validaciones con express validator > manejo de errores con express validator. all in middlewares   
+        res.status(201).json({
+            ok: true,
+            msj: 'new',
+        });
+    } catch (error) {
+        console.log(error);
+        res.status( 500).json({
+            ok : false,
+            msj : 'Por favor comuniquese con el administrador'
+        });
+    }
 };
 
-const USER_LOGIN = ( req, res = response )=>{
-    const { email, password } = req.body;  
+const USER_LOGIN = (req, res = response) => {
+    const { email, password } = req.body;
     //el manejo de errores lo hacemos con un custom middleware. file validar-campos.js
-    res.status( 201 ).json({
-        ok : true,
-        msg : 'login',
+    res.status(201).json({
+        ok: true,
+        msg: 'login',
         email,
         password,
     });
 };
 
-const RENEW_TOKEN = ( req, res = response  )=>{
+const RENEW_TOKEN = (req, res = response) => {
     res.json({
-        ok : true,
-        msg : 'renew token',
+        ok: true,
+        msg: 'renew token',
     });
 };
 
--- database/config.js
+++ database/config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const dbConnection=()=>{
+const dbConnection=async()=>{
     try {
         mongoose.connect(process.env.DB_CNN);
         console.log('DB On line');
     } catch( error ){
-        console.error( error );
+        console.log( error );
         throw new Error('Error a la hora de iniciar base de datos');
     }
 };
